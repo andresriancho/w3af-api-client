575d69dd5b74fbf6b53949566b67daa729dc94e3
Andres Riancho <andres.riancho@gmail.com>
2015-06-23 21:05:46 -0300

    https://circleci.com/gh/andresriancho/w3af-api-client/19

--- w3af_api_client/__init__.py
+++ w3af_api_client/__init__.py
@@ -1,8 +1,12 @@
 __VERSION__ = '0.0.1'
 
-from .connection import Connection
-from .finding import Finding
-from .log import Log, LogEntry
-from .scan import Scan
+try:
+    from .connection import Connection
+    from .finding import Finding
+    from .log import Log, LogEntry
+    from .scan import Scan
+except ImportError:
+    # https://circleci.com/gh/andresriancho/w3af-api-client/19
+    pass
 
 
