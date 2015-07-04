1605c4483409c56cf976b3d9cbc696c962372572
Andres Riancho <andres.riancho@gmail.com>
2015-07-04 11:48:51 -0300

    Include class in init

--- w3af_api_client/__init__.py
+++ w3af_api_client/__init__.py
@@ -5,6 +5,7 @@ try:
     from .finding import Finding
     from .log import Log, LogEntry
     from .scan import Scan
+    from .scanner_exception import ScannerException
 except ImportError:
     # https://circleci.com/gh/andresriancho/w3af-api-client/19
     pass
